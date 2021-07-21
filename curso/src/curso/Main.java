ed75959eae4ab255553c47631444628f1693f188
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-21 18:49:48 -0500

    tiposda

---
+++ curso/src/curso/Main.java
@@ -0,0 +1,18 @@
+package curso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		boolean A= true;
+		short cantidadestud= 1000;
+		byte entrdadecircuito=0; //o 1
+		int entero = 1;
+		long largo = 1000000000;
+		double saldodebanco= 2000000.12;
+		float decimal =  2.5f;
+		char caracter= 'a';
+		
+	}
+
+}
